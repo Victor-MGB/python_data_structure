c82ffa3ed29552b6b8bb0b534a510a4f1dcd171a
Victor-MGB <mgbemenaosonduv@gmail.com>
2024-08-13 00:08:11 +0100

    another from python ds

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+# A dictionary of friends and their favorite colors
+favorite_colors = {
+    "Alice": "blue",
+    "Bob": "green",
+    "Charlie": "red"
+}
+
+# Let's see what's inside
+print(favorite_colors["Bob"])  # Output: {"Alice": "blue", "Bob": "green", "Charlie": "red"}
---
+++ tuple.py
@@ -0,0 +1,4 @@
+favorite_snack = ('cream crackers','cookies','digestive biscuit')
+
+first_snack = len(favorite_snack)
+print(first_snack)
